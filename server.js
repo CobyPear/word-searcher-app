333129388290e6e069358dcc91160b16fcf8af72
_Pear <63015754+CobyPear@users.noreply.github.com>
2021-02-27 16:41:15 -0600

    added mongoose connect to server.js

--- server.js
+++ server.js
@@ -7,5 +7,10 @@ dotenv.config()
 const PORT = process.env.PORT || 8080
 const app = express()
 
+mongoose.connect( process.env.MONGO_URI || "mongodb://localhost/worddb", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
 
 app.listen(PORT, console.log(`Server running on port ${PORT}`))
