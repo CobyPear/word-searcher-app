5ecb30d87633c22b65d1fa155fc0e738aa8b2c41
_Pear <63015754+CobyPear@users.noreply.github.com>
2021-02-27 16:27:57 -0600

    boilerplate server.js

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import logger from 'morgan'
+
+dotenv.config()
+
+const PORT = process.env.PORT || 8080
+const app = express()
+
+
+app.listen(PORT, console.log(`Server running on port ${PORT}`))
