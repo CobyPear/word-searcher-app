4b589687c4bad0657cc057a9829631b813b8fc89
_Pear <63015754+CobyPear@users.noreply.github.com>
2021-02-27 16:53:01 -0600

    added a seeder.js file to test our database

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import logger from 'morgan'
+import mongoose from 'mongoose'
 
 dotenv.config()
 
 const PORT = process.env.PORT || 8080
 const app = express()
 
-mongoose.connect( process.env.MONGO_URI || "mongodb://localhost/worddb", {
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('client'));
+
+mongoose.connect('mongodb://localhost/worddb', {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
   useFindAndModify: false
 });
 
